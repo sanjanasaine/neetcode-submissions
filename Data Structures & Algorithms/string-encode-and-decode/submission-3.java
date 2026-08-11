ba7f4a7f55cc6e1331fccafbfa853be45dc159cd
Sanjana Saini <itssanjanasaine@gmail.com>
2026-08-11 18:36:03 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    public String encode(List<String> strs) 
+    {
+        // encode the string length + seperator + string
+        String encoded ="";
+        for(int i = 0 ; i < strs.size(); i++)
+        {
+            encoded = encoded + strs.get(i).length() + "#" + strs.get(i);
+        }
+
+        return encoded;
+    }
+
+    public List<String> decode(String str) 
+    {
+
+        //decode the string
+        ArrayList<String> ans = new ArrayList<>();
+        int i = 0;
+        while( i < str.length())
+        {   
+            String number = "";
+            
+            while(str.charAt(i) != '#')
+            {
+               number = number + str.charAt(i);
+               i++;
+            }
+            int num =Integer.parseInt(number);
+
+            int count = 0;
+            String word = "";
+            int j = i + 1;
+            while(count < num)
+            {
+                word = word + str.charAt(j);
+                count++;
+                j++;
+            }
+
+            ans.add(word);
+            i= j;
+        }
+       
+     return ans;
+
+    }
+}
