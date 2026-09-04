d66efcf1a8c5fa2bf9a14a9b56255c7a2525f2a7
Sanjana Saini <itssanjanasaine@gmail.com>
2026-09-04 12:57:10 +0530

    Add: subtree-of-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-3.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {  
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) 
+    {
+        if(subRoot == null)
+          return true;
+
+        if(root == null)
+           return false;
+
+        if(isSameTree(root, subRoot))
+           return true;
+
+        if(isSubtree(root.left, subRoot))
+           return true;
+
+         return isSubtree(root.right, subRoot);    
+                
+    }
+
+    public boolean isSameTree(TreeNode root, TreeNode subRoot)
+    {
+      if(root == null && subRoot == null)
+        return true;
+
+      if(root == null || subRoot == null)
+        return false;
+
+      if(root.val != subRoot.val)
+         return false;
+
+
+      return isSameTree(root.left , subRoot.left) && isSameTree(root.right, subRoot.right);       
+    }
+}
